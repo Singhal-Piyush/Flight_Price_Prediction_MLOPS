be269c141d9b8c7cad806b742571bf9891191372
Singhal-Piyush <singhalpiyush3610@gmail.com>
2024-12-05 00:17:39 +0530

    Removing error in folder structure

--- template.py
+++ template.py
@@ -14,11 +14,11 @@ list_of_files = [
     "src/pipeline/__init__.py",
     "src/pipeline/training_pipeline.py",
     "src/pipeline/prediction_pipeline.py",
-    "src/utils/__init__.py"
+    "src/utils/__init__.py",
     "src/utils/utils.py",
     "src/logger/__init__.py",
     "src/logger/logging.py",
-    "src/exception/__init__py",
+    "src/exception/__init__.py",
     "src/exception/exception.py",
     "tests/unit/__init__.py",
     "tests/integration/__init__.py",
